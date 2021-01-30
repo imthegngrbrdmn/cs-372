1f5e82aee84192485289ba982bbfbfa66353e76f
tylercchase <tylercchase@gmail.com>
2021-01-29 16:20:12 -0900

    wip: Comma delimited adding

--- TDD-Kata/string_calculator.cpp
+++ TDD-Kata/string_calculator.cpp
@@ -6,6 +6,18 @@ int stringCalculator(std::string input)
 	{
 		return 0;
 	}
+	//Two numbers, comma delimited, returns the sum
+	std::string left = "";
+	std::string right = "";
+	std::string delimeter = ",";
+	bool split = false;
+	for (int i = 0; i < input.length(); i++)
+	{
+		if(!split && delimeter.compare((std::string)input[i]) == 0){
+			split = true;
+		}
+	}
+	
 	return std::stoi(input);
 }
 
--- TDD-Kata/test.cpp
+++ TDD-Kata/test.cpp
@@ -7,9 +7,16 @@ TEST_CASE("An empty string returns zero")
 }
 TEST_CASE("Single number returns value")
 {
+	REQUIRE(stringCalculator("0") == 0);
 	REQUIRE(stringCalculator("1") == 1);
 	REQUIRE(stringCalculator("2") == 2);
 	REQUIRE(stringCalculator("3") == 3);
 	REQUIRE(stringCalculator("100") == 100);
 	REQUIRE(stringCalculator("1000") == 1000);
+}
+TEST_CASE("Two numbers, comma delimited, returns the sum"){
+	REQUIRE(stringCalculator("1,2") == 3);
+	REQUIRE(stringCalculator("2,2") == 4);
+	REQUIRE(stringCalculator("50,50") == 100);
+	REQUIRE(stringCalculator("0,0") == 0);
 }
