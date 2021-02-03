c5f7b5f1b45077b5274c03215ea7576eba3fbae3
Millard Arnold V <clear.the.spam@gmail.com>
2021-02-02 15:01:09 -0900

    More work on addition

--- TDD-Kata/string_calculator.cpp
+++ TDD-Kata/string_calculator.cpp
@@ -13,11 +13,24 @@ int stringCalculator(std::string input)
 	bool split = false;
 	for (int i = 0; i < input.length(); i++)
 	{
-		if(!split && delimeter.compare((std::string)input[i]) == 0){
+		if(!split && delimeter.compare(input.substr(i,i+1)) == 0)
+		{
 			split = true;
+			continue;
+		}
+		if (!split)
+		{
+			left += input.substr(i, i + 1);
+		}
+		else
+		{
+			right += input.substr(i, i + 1);
 		}
 	}
-	
+	if (split)
+	{
+		return std::stoi(left) + std::stoi(right);
+	}
 	return std::stoi(input);
 }
 
