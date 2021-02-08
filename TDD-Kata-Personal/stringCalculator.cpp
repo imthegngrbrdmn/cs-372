31fb91f0deed59420bf4c88bcbde604ed751b255
imthegngrbrdmn <clear.the.spam@gmail.com>
2021-02-07 17:45:47 -0900

    A single char delimiter can be defined on the first line - PASSED

--- TDD-Kata-Personal/stringCalculator.cpp
+++ TDD-Kata-Personal/stringCalculator.cpp
@@ -8,13 +8,30 @@ int strCalc(std::string input)
     }
     std::vector<std::string> nums = { "" };
     std::size_t i = 0;
+    char userdel = '\n';
+    try
+    {
+        std::stoi(std::string(1,input[0]));
+    }
+    catch (...)
+    {
+        if (input[1] == '\n')
+        {
+            userdel = input[0];
+            input = input.substr(2);
+        }
+        else
+        {
+            throw std::logic_error("Unknown");
+        }
+    }
     for (char c : input)
     {
         if (c == '-')
         {
             throw std::logic_error("Negative numbers are not allowed");
         }
-        if (c == ',' || c == '\n')
+        if (c == ',' || c == '\n' || c == userdel)
         {
             ++i;
             nums.push_back("");
