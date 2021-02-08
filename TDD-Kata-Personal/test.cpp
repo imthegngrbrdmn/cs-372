45ce7c0db1272df6af64ee7e6dd01c14b6e6b958
imthegngrbrdmn <clear.the.spam@gmail.com>
2021-02-07 16:34:46 -0900

    Two numbers, comma delimited, returns the sum - Test

--- TDD-Kata-Personal/test.cpp
+++ TDD-Kata-Personal/test.cpp
@@ -43,6 +43,38 @@ TEST_CASE("A single number returns the value")
         REQUIRE(strCalc("1234567890") == 1234567890);
     }
 }
+TEST_CASE("Two numbers, comma delimited, returns the sum")
+{
+    SUBCASE("Single digits")
+    {
+        REQUIRE(strCalc("0,1") == 1);
+        REQUIRE(strCalc("1,1") == 2);
+        REQUIRE(strCalc("1,2") == 3);
+        REQUIRE(strCalc("2,3") == 5);
+        REQUIRE(strCalc("3,5") == 8);
+    }
+    SUBCASE("Double digits")
+    {
+        REQUIRE(strCalc("10,11") == 21);
+        REQUIRE(strCalc("13,31") == 44);
+        REQUIRE(strCalc("21,12") == 33);
+        REQUIRE(strCalc("72,43") == 115);
+        REQUIRE(strCalc("63,75") == 138);
+    }
+    SUBCASE("Three or more digits")
+    {
+        REQUIRE(strCalc("111,222") == 333);
+        REQUIRE(strCalc("1234,1234") == 2468);
+        REQUIRE(strCalc("1567,2289") == 3856);
+        REQUIRE(strCalc("200000,345678") == 545678);
+    }
+    SUBCASE("Mixes")
+    {
+        REQUIRE(strCalc("10000,45") == 10045);
+        REQUIRE(strCalc("1,1000") == 1001);
+        REQUIRE(strCalc("123,1") == 124);
+    }
+}
 
 int main(int argc, char** argv)
 {
