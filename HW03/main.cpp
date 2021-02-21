efd592ffad5f5723afdada9c3d7c82f26c4f13b2
Millard A. Arnold V <clear.the.spam@gmail.com>
2021-02-20 17:08:47 -0900

    Randomize fibonacci numbers then sort & print with lambda functions

--- HW03/main.cpp
+++ HW03/main.cpp
@@ -2,13 +2,18 @@
 #include <vector>
 #include <algorithm>
 #include <cstdint>
+#include <random>
 
 int main(int argc, char**argv)
 {
   std::vector<uint_fast64_t> fibs{0,1};
-  for(int i = 0; i < 255; ++i) {
+  for(std::size_t i = 0; i < 93; ++i) {
     fibs.push_back(fibs[i]+fibs[i+1]);
   }
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::shuffle(fibs.begin(), fibs.end(), gen);
+  std::sort(fibs.begin(), fibs.end(), [](uint_fast64_t a, uint_fast64_t b) { return a>b; });
   std::for_each(fibs.begin(), fibs.end(), [](uint_fast64_t& n) { std::cout << n << std::endl; });
 	return 0;
 }
