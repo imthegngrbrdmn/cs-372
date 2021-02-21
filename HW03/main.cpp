01caf01ecd9138f8045db45c1a3ae48b502cb096
Millard A. Arnold V <clear.the.spam@gmail.com>
2021-02-20 16:36:49 -0900

    Printing first 256 digits of fibonacci sequence

--- HW03/main.cpp
+++ HW03/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
+#include <cstdint>
 
 int main(int argc, char**argv)
 {
+  std::vector<uint_fast64_t> fibs{0,1};
+  for(int i = 0; i < 255; ++i) {
+    fibs.push_back(fibs[i]+fibs[i+1]);
+  }
+  std::for_each(fibs.begin(), fibs.end(), [](uint_fast64_t& n) { std::cout << n << std::endl; });
 	return 0;
 }
